ca12cd677de0eecd1bbb89f1a9bf821f3dcab351
Andrew <andrewespn@hotmail.com>
2019-01-28 15:17:18 -0800

    Updated index.html and functions.js so the html document will now take user input and compute output accordingly

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,7 @@
-function addition(num1, num2) {
-    return num1 + num2
+function addition() {
+    var num1 = document.getElementById("Number1").value;
+    var num2 = document.getElementById("Number2").value;
+    var sum = Number(num1) + Number(num2);
+
+    document.getElementById("Output").innerHTML = sum;
 }
